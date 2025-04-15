126e3fda9b4fc22c04b6554022f25371cdea1e7a
Booruno <1brunoocunha@gmail.com>
2025-04-15 15:12:35 -0300

    Testes em dispositos mobiles

--- cypress/e2e/desafio_cadastro_usuarios.cy.js
+++ cypress/e2e/desafio_cadastro_usuarios.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-import { faker } from '@faker-js/faker'
+import { faker, he } from '@faker-js/faker'
 import homePage from '../support/pages/home_page'
 import registerPage from '../support/pages/register_page'
 
@@ -9,39 +9,47 @@ const user_password = faker.internet.password({ length: 8 }) // Gera uma senha a
 
 const user_data = require('../fixtures/desafio.json')
 
-describe ('Cadastro de Usuários', () => {
 
-  beforeEach(() => {
-    homePage.accesRegisterPage()
+const screens = require('../fixtures/screens.json') // Importando o arquivo JSON com os tamanhos de tela
+
+screens.forEach(({device, width, height}) => { //Função para iterar entre os elementos do array
+  describe ('Cadastro de Usuários', () => {
+
+    beforeEach(() => {
+      if(device != 'desktop') {
+        cy.viewport(width, height) // Aqui podemos definir o tamanho da tela
+      }
+      homePage.accesRegisterPage()
+    })
+
+    it('Cadastro de usuários com sucesso', () => {
+      registerPage.fillName(user_name)
+      registerPage.fillEmail(user_email)
+      registerPage.fillPassword(user_password)
+      registerPage.saveRegister()
+      registerPage.checkSuccessful()
+
+    })
+
+    it('Validar campo nome vazio', () => {
+      registerPage.fillEmail('booruno@gmail.com')
+      registerPage.fillPassword('12345678')
+      registerPage.saveRegister()
+      registerPage.checkMessage('O campo nome deve ser prenchido', 'Campo nome vazio validado com sucesso')
+    })
+
+    it('Validar campo email vazio', () => {
+      registerPage.fillName(user_data.user_name)
+      registerPage.fillPassword(user_data.user_password)
+      registerPage.saveRegister()
+      registerPage.checkMessage('O campo e-mail deve ser prenchido', 'Campo e-mail vazio validado com sucesso')
+    })
+
+    it('Validar campo senha inválida', () => {
+      registerPage.fillName(user_data.user_name)
+      registerPage.fillEmail(user_data.user_email)
+      registerPage.saveRegister()
+      registerPage.checkMessage('O campo senha deve ter pelo menos 6 dígitos', 'Campo senha inválida validado com sucesso')
+    })
   })
-
-  it('Cadastro de usuários com sucesso', () => {
-    registerPage.fillName(user_name)
-    registerPage.fillEmail(user_email)
-    registerPage.fillPassword(user_password)
-    registerPage.saveRegister()
-    registerPage.checkSuccessful()
-
-  })
-
-  it('Validar campo nome vazio', () => {
-    registerPage.fillEmail('booruno@gmail.com')
-    registerPage.fillPassword('12345678')
-    registerPage.saveRegister()
-    registerPage.checkMessage('O campo nome deve ser prenchido', 'Campo nome vazio validado com sucesso')
-  })
-
-  it('Validar campo email vazio', () => {
-    registerPage.fillName(user_data.user_name)
-    registerPage.fillPassword(user_data.user_password)
-    registerPage.saveRegister()
-    registerPage.checkMessage('O campo e-mail deve ser prenchido', 'Campo e-mail vazio validado com sucesso')
-  })
-
-  it('Validar campo senha inválida', () => {
-    registerPage.fillName(user_data.user_name)
-    registerPage.fillEmail(user_data.user_email)
-    registerPage.saveRegister()
-    registerPage.checkMessage('O campo senha deve ter pelo menos 6 dígitos', 'Campo senha inválida validado com sucesso')
-  })
-})
+});
